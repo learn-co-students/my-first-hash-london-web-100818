fc0a32a0a4e95dfdbeddd37eecbe9baa876f20fc
Bridget Kalinda <bridgetkalinda@yahoo.co.uk>
2018-10-08 14:22:51 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"name" => "Bridget", "age" => 23, "location" => "London"}
+  my_hash = {"name" => "Bridget", "age" => 23, "location" => "Croydon"}
 
 end
 
@@ -37,10 +37,13 @@ shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil pai
 
 
   # add 4 gun powder to the shipping_manifest hash below
-shipping_manifest["pearl necklace"] = 1
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
+
+  shipping_manifest
 
   shipping_manifest
 
